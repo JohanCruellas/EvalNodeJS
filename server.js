c21f2c4a2896b96980eb4b45fe0f93e37e4252c6
Jo-han <jo-han@miloctav.fr>
2023-10-27 10:03:31 +0200

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { createServer } from 'node:http'
+import { readFileSync, writeFileSync } from 'node:fs';
+
+const students = [
+    { name : "Sonia", birth : "2019-14-05"},
+    { name : "Antoine", birth : "2000-12-05"},
+    { name : "Alice", birth : "1990-14-09"},
+    { name : "Sophie", birth : "2001-10-02"},
+    { name : "Bernard", birth : "1980-21-08"}
+];
+
+const server = createServer((request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    if (request.url === '/') {
+        response.end(readFileSync('./view/home.html'));
+    }
+});
+
+
+server.listen(process.env.APP_PORT, () => {
+    console.log(process.env)
+    console.log(`Server started on port ${process.env.APP_PORT}`);
+});
+
