57720988bd1dff32306a09ca66fb4b771b399c6b
Jo-han <jo-han@miloctav.fr>
2023-10-27 16:45:37 +0200

    styling, env variables, date format

--- server.js
+++ server.js
@@ -6,53 +6,69 @@ import { readFileSync } from 'node:fs';
 import * as url from 'node:url';
 
 const server = createServer((request, response) => {
+
     response.setHeader('Content-Type', 'text/html');
-    if (request.url === '/') {
-        response.end(readFileSync('./view/home.html'));
-    }
 
-    else if (request.url === '/addStudent' && request.method === 'POST') {
-        const body = [];
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            let args = parsedBody.split('&');
+    if (request.url === '/style.css') {
+        const cssPath = './assets/css/style.css';
+        response.setHeader('Content-Type', 'text/css');
+        response.end(readFileSync(cssPath));
+    } else {
 
-            let name = args[0].split('=')[1];
-            let dob = args[1].split('=')[1];
-
-            addStudent(name, dob);
-
-            response.statusCode = 302;
-            response.setHeader('Location', '/');
-            return response.end();
-        });
-    }
-
-    else if (url.parse(request.url).pathname === '/deleteStudent' && request.method === 'POST') {
-
-        const index = url.parse(request.url, true).query.index;
-
-        if (isNaN(index)) {
-            response.statusCode = 400;
-            return response.end();
+        if (request.url === '/') {
+            response.end(readFileSync('./view/home.html'));
         }
 
-        deleteStudent(index);
-        
-        response.statusCode = 302;
-        response.setHeader('Location', '/students');
-        return response.end();
-    }
+        else if (request.url === '/addStudent' && request.method === 'POST') {
+            const body = [];
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+            request.on('end', () => {
+                const parsedBody = Buffer.concat(body).toString();
+                let args = parsedBody.split('&');
 
-    else if (request.url === '/students') {
-        response.end(generateStudentsPage());
-    }
+                let name = args[0].split('=')[1];
+                let dob = args[1].split('=')[1];
 
-    else {
-        response.end(readFileSync('./view/404.html'))
+                if (name === '' || dob === '' || isNaN(Date.parse(dob))) {
+                    response.statusCode = 400;
+                    console.error('Invalid name or date of birth');
+                    return response.end(readFileSync('./view/home.html'));
+                } else {
+                    addStudent(name, dob);
+                    response.statusCode = 302;
+                    response.setHeader('Location', '/');
+                    return response.end();
+                }
+            });
+        }
+
+        else if (url.parse(request.url).pathname === '/deleteStudent' && request.method === 'POST') {
+
+            const index = url.parse(request.url, true).query.index;
+
+            if (isNaN(index) || index < 0 || index >= students.length) {
+                response.statusCode = 400;
+                console.error('Invalid index');
+                return response.end(readFileSync('./view/home.html'));
+            } else {
+
+                deleteStudent(index);
+
+                response.statusCode = 302;
+                response.setHeader('Location', '/students');
+                return response.end();
+            }
+        }
+
+        else if (request.url === '/students') {
+            response.end(generateStudentsPage());
+        }
+
+        else {
+            response.end(readFileSync('./view/404.html'))
+        }
     }
 });
 
--- utils.js
+++ utils.js
@@ -1,25 +1,41 @@
 import { students } from './data/data.js';
+import dayjs from 'dayjs';
 
 function generateStudentsPage() {
     let html = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Students Page</title>
+    <link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<body>
+<div class="card">
+
     <nav>
         <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/students">Students</a></li>
+            <li class="activeTab"><a href="/students">Students</a></li>
         </ul>
     </nav>
-    <ul>`;
+    <ul class="addForm">`;
     for (let [studentIndex, student] of students.entries()) {
         html += `
         <form action="/deleteStudent?index=${studentIndex}" method="POST">
-            <li>${student.name} - ${student.birth}
-                <button type="submit" value="Submit">Delete</button>
+            <li class="studentItem">${student.name} - ${dayjs(student.birth).format('DD/MM/YYYY')}
+                <button type="submit" value="Submit" class="btn">X</button>
             </li>
         </form> `;
     }
-    html += '</ul>';
+    html += `
+                </ul>
+                </div>
+            </body>
+        </html>`;
     return html;
 }
+
 
 function addStudent(name, dob) {
     students.push({ name, birth: dob });
